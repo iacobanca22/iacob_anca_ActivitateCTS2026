9b7b2841ce12189c876f66bd25acc7de014c6bc8
iacobanca22 <iacobanca22@stud.ase.ro>
2026-03-23 23:11:57 +0200

    Delete ase.cts.ro/adapter/restaurant/src directory

--- ase.cts.ro/adapter/restaurant/src/clase/SistemNouRestaurant.java
+++
@@ -1,7 +0,0 @@
-package clase;
-
-public class SistemNouRestaurant {
-    public void afiseazaBon() {
-        System.out.println("Bon afisat din sistemul nou.");
-    }
-}
--- ase.cts.ro/adapter/restaurant/src/clase/SistemVechi.java
+++
@@ -1,9 +0,0 @@
-package clase;
-
-public class SistemVechi implements TargetFactura{
-
-    @Override
-    public void printeazaFactura() {
-        System.out.println("Factura printata din sistemul vechi.");
-    }
-}
